6fb68fb630db20d4de0d0d57679b2fd8bde9b5df
Karla Elizabeth Lopez <karlselizabethlopez@gmail.com>
2023-02-08 20:23:01 -0600

    Cambio
    Cambio y tarea

--- 230125-spotify/basic-server/main.py
+++ 230125-spotify/basic-server/main.py
@@ -1,24 +1,18 @@
-import webbrowser
-from urllib.parse import urlencode
 from my_server import MyServer
 from http.server import HTTPServer
 
-
-client_id = '2d7800dcec2241389843e017b8ab5e4d'
-client_secret = 'e8f0ce222da24f62881d26db6c2b0291'
-auth_headers = {
-    'client_id': client_id,
-    'response_type': 'code',
-    'redirect_uri': 'http://localhost:7777/callback',
-    'scope': 'playlist-modify-public'
-}
-url = 'https://accounts.spotify.com/authorize?'
-
-webbrowser.open(url + urlencode(auth_headers))
 
 web_server = HTTPServer(
     ('localhost', 7777),
     MyServer
 )
+
 print('Iniciando Servidor...')
-web_server.serve_forever()
+try:
+    web_server.serve_forever()
+except KeyboardInterrupt:
+    print('Apagando el servidor...')
+
+
+print('El servidor se ha detenido.')
+print('Adios')
--- 230125-spotify/basic-server/my_server.py
+++ 230125-spotify/basic-server/my_server.py
@@ -1,16 +1,29 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-
+import webbrowser
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = parse.urlparse(self.path)
-        if parsed_path.path == '/callback':
+
+        if parsed_path.path == '/authorize':
+            client_id = '3u8j2XwGGhJjNbwB2tekad'
+            auth_headers = {
+                'client_id': client_id,
+                'response_type': 'code',
+                'redirect_uri': 'http://localhost:7777/callback',
+                'scope': 'playlist-modify-public'
+            }
+            url = 'https://accounts.spotify.com/authorize?'
+            webbrowser.open(url + parse.urlencode(auth_headers))
+
+        elif parsed_path.path == '/callback':
             query = parsed_path.query
             code = query.split('=')[1]
             f = open('../code.txt', 'w')
             f.write(code)
             f.close()
-        else:
-            print("No hacer nada")
-        self.send_response(200, 'Todo bien')
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
