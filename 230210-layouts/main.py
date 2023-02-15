9b0d44dcf55ac2196283369f4c59c31f4df2bf02
Karla Elizabeth Lopez <karlselizabethlopez@gmail.com>
2023-02-14 23:12:35 -0600

    Initial Commit
    .

--- 230210-layouts/main.py
+++ 230210-layouts/main.py
@@ -1,16 +1,23 @@
 import sys
 from PyQt6.QtWidgets import QApplication
-from main_window import MainWindow
+from main_window import MainWindow, MainWindow2, MainWindow3, MainWindow4, MainWindow5
 
 
 app = QApplication(sys.argv)
 
-window = MainWindow()
-window.show()
+v_window = MainWindow()
+v_window.show()
 
-hwindow = MainWindow2()
-hwindow.show()
+h_window = MainWindow2()
+h_window.show()
 
+vh_window = MainWindow3()
+vh_window.show()
 
+g_window = MainWindow4()
+g_window.show()
+
+s_window = MainWindow5()
+s_window.show()
 
 app.exec()
--- 230210-layouts/main_window.py
+++ 230210-layouts/main_window.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QHBoxLayout
+from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QHBoxLayout, QGridLayout, QStackedLayout
 from color_widget import ColorWidget
 
 
@@ -11,7 +11,7 @@ class MainWindow(QMainWindow):
         layout.addWidget(ColorWidget('red'))
         layout.addWidget(ColorWidget('brown'))
         layout.addWidget(ColorWidget('black'))
-        layout.addWidget(ColorWidget('skyblue'))
+        layout.addWidget(ColorWidget('sky blue'))
         layout.setContentsMargins(0, 0, 0, 0)
         layout.setSpacing(0)
 
@@ -26,21 +26,86 @@ class MainWindow2(QMainWindow):
 
         self.setWindowTitle('Layouts')
 
+        layout = QHBoxLayout()
+        layout.addWidget(ColorWidget('gray'))
+        layout.addWidget(ColorWidget('violet'))
+        layout.addWidget(ColorWidget('chocolate'))
+        layout.addWidget(ColorWidget('light green'))
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+
+class MainWindow3(QMainWindow):
+    def __init__(self):
+        super(MainWindow3, self).__init__()
+
+        self.setWindowTitle('Layouts')
+
         layout1 = QHBoxLayout()
         layout2 = QVBoxLayout()
         layout3 = QVBoxLayout()
 
-        layout1.setContentsMargins(0,0,0,0)
-        layout1.setSpacing(20)
-
         layout2.addWidget(ColorWidget('red'))
         layout2.addWidget(ColorWidget('yellow'))
         layout2.addWidget(ColorWidget('purple'))
+
+        layout1.addLayout(layout2)
+
         layout1.addWidget(ColorWidget('green'))
 
         layout3.addWidget(ColorWidget('red'))
         layout3.addWidget(ColorWidget('purple'))
 
+        layout1.addLayout(layout3)
+        layout1.setContentsMargins(0, 0, 0, 0)
+        layout1.setSpacing(0)
+
         widget = QWidget()
         widget.setLayout(layout1)
+        self.setCentralWidget(widget)
+
+
+class MainWindow4(QMainWindow):
+    def __init__(self):
+        super(MainWindow4, self).__init__()
+
+        self.setWindowTitle('Layouts')
+
+        layout = QGridLayout()
+
+        layout.addWidget(ColorWidget('turquoise'), 0, 3)
+        layout.addWidget(ColorWidget('light green'), 1, 1)
+        layout.addWidget(ColorWidget('light blue'), 2, 2)
+        layout.addWidget(ColorWidget('dark blue'), 3, 0)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+
+class MainWindow5(QMainWindow):
+    def __init__(self):
+        super(MainWindow5, self).__init__()
+
+        self.setWindowTitle('Layouts')
+
+        layout = QStackedLayout()
+
+        layout.addWidget(ColorWidget("red"))
+        layout.addWidget(ColorWidget("green"))
+        layout.addWidget(ColorWidget("blue"))
+        layout.addWidget(ColorWidget("yellow"))
+
+        layout.setCurrentIndex(1)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+
+        widget = QWidget()
+        widget.setLayout(layout)
         self.setCentralWidget(widget)
