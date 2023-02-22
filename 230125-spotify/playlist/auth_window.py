fa020bf3dff29d14cfd5b086c5836f92b67b77af
Karla Elizabeth Lopez <karlselizabethlopez@gmail.com>
2023-02-21 21:49:20 -0600

    Initial Commit
    .

--- 230125-spotify/playlist/auth_window.py
+++ 230125-spotify/playlist/auth_window.py
@@ -5,18 +5,15 @@ class AuthWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         login_button = QPushButton('Ingresar')
-        login_button.cliked.connect(self.open_browser)
-
         refresh_button = QPushButton('Volver a intentar')
-        refresh_button.clicked.connect(self.try_again)
 
         layout = QVBoxLayout()
         layout.addWidget(login_button)
         layout.addWidget(refresh_button)
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.setSpacing(0)
 
         widget = QWidget()
         widget.setLayout(layout)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
         self.setCentralWidget(widget)
 
