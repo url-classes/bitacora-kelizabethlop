83b4128a423345f251b1fdd74f7baaec7755931e
Karla Elizabeth Lopez <karlselizabethlopez@gmail.com>
2023-03-01 09:05:10 -0600

    Initial Commit
    .

---
+++ 230301-Listas/circular_list.py
@@ -0,0 +1,9 @@
+from node import Node
+
+
+class CircularList:
+    def __init__(self):
+        self.head: Node / None = None
+        self.tail: Node / None = None
+        self.size: int = 0
+def prepend(self, data: init):
---
+++ 230301-Listas/node.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self, data: init):
+        self.data = data
+        self.next None / None = None
+
